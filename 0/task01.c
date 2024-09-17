9195763a008c4bc6568bf35cd4487d3a4c57b2d2
Mikhail Ananyevskiy <msaipme@gmail.com>
2024-09-17 13:15:35 +0300

    task01 update

--- 0/task01.c
+++ 0/task01.c
@@ -1,21 +1,61 @@
 #include<stdio.h>
 
-typedef long long int Money;
+typedef long long int Money;  // RUR
+
+struct Mortgage {
+    double rate;
+    Money credit;
+    Money platez;
+    Money month_pay;
+};
 
 
 struct Person {
     Money salary;
     Money account;
+    Money food;
+    Money expences;
+    struct Mortgage mortgage;
 };
 
 
-struct Person alice;
+struct Person alice;  // mortgage
+struct Person bob;
+
+void alice_mortgage()
+{
+    alice.account -= alice.mortgage.month_pay;
+    
+}
+
+void alice_food(const int month)
+{
+    if (month == 1) {
+        alice.food *= 1.1;
+    }
+    alice.account -= alice.food;
+}
+
+void alice_expences(const int month ){
+    if (month == 1) {
+        alice.account -= 5000;
+    }
+    alice.account -= alice.expences;
+}
 
 
 void alice_init()
 {
     alice.account = 1000 * 1000;
     alice.salary = 200 * 1000;
+    alice.food = 10 * 1000;
+    alice.expences = 70 * 1000;
+
+    alice.mortgage.platez = 1000 * 1000;
+    alice.mortgage.credit = 14000 * 1000;
+    alice.mortgage.rate = 0.17;
+    alice.mortgage.month_pay = 100 * 1000;  //
+    alice.account -= alice.mortgage.platez;
 }
 
 
@@ -45,9 +85,9 @@ void simulation()
     while( !((month == 9) && (year == 2024 + 30)) ) {
 
         alice_salary(month, year);
-        // alice_mortgage();
-        // alice_food();
-        // alice_expences();
+        alice_mortgage();
+        alice_food(month);
+        alice_expences(month );  // одежда, личные вещи
         // alice_car();
         // alice_vacation();
 
