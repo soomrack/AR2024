d91ebfe4a1b39691b7b85130b36f2f4bd22985c3
sirok33 <100148214+sirok33@users.noreply.github.com>
2024-09-22 18:16:47 +0300

    Update main.cpp(task1)

--- petrishchevSV/main.cpp
+++ petrishchevSV/main.cpp
@@ -1,51 +1,58 @@
 #include <stdio.h>
 #include <cmath>
+#include <algorithm>
+#define __max(a,b)  (((a) > (b)) ? (a) : (b))
+#define __min(a,b)  (((a) < (b)) ? (a) : (b))
 
 //start networth = 1000000 price of apartment = 13000000
 
-float stratAlice(float networth, float infliation,float pay,float salary,float index_salary,float food,float n)
+int stratAlice(float networth, float infliation,float pay,float salary,float index_salary,float food,float n, float price)
 {
     networth = 0;
     for(int i=0; i<n; i++)
     {
         
-        networth = networth+12*((salary*index_salary)-food*infliation - pay);
+        networth = networth + (12*((salary*index_salary)-food*infliation - pay));
         
 
     }
-    networth = networth + 172479820;    return(networth);
+    networth = networth + pow(price,n);    return(networth);
 
 }
-float stratBOB(float networth, float infliation,float pay,float salary,float index_salary,float food,float n)
+int stratBOB(float bank, float bank_percents, float infliation,float pay,float salary,float index_salary,float food,float n)
 {
+float networth;
 for(int i=0; i<n; i++)
     {
-        
-        networth = networth + 12*((salary*index_salary)-food*infliation - pay);
-
+        bank = bank*bank_percents;
+        networth = 12*((salary*index_salary)-food*infliation - pay);
+        bank = bank + networth;
+        networth = 0;
 
     }
-        return(networth);
+        return(networth+bank);
 
     
 }
-void zalypa(){
 
-
-    printf("penis\n");
-}
 int main() 
 {
-printf("Введите начальный капитал");
+printf("hello");
 
-float bob,al;
-float k;
-zalypa();
-al = stratAlice(0,1.09,150000,200000,1.09,50000,30);
-printf("%f \n", al );
-bob = stratBOB(1000000,1.09,30000,200000,1.09,50000,30);
-printf("%f \n", bob );
+int bob,al;
+
+
+al = stratAlice(0,1.09,150000,200000,1.09,50000,30,13e6);
+
+
+bob = stratBOB(1e6, 1.2, 1.09,30000,200000,1.09,50000,30);
+printf("\nAlice ""%i ", al );
+printf("\nBob ""%i ", bob );
+//костыль для оценки стратегий
+float s = bob/al;
+float b0b = bob;
+float all = al;
+if (__min(all,b0b)/__max(all,b0b)>0.95) printf("\nboth of these strategics are good");
+
 }
-
-
 
