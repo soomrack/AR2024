fc35e02e78f073e65954a62ef84d3217f02c2eec
Agent112200393 <kolyagoryachun@gmail.com>
2024-09-23 18:47:50 +0300

    Переделано первое задание

--- Горячун Николай Дмитриевич/Task 1/task1.c
+++ Горячун Николай Дмитриевич/Task 1/task1.c
@@ -1,45 +1,77 @@
 #include <stdio.h>
-void main(){
-    typedef long long int Money;
-	Money expenditure = 399 + 22500 + 5000 + 4500;
-	long double inflation = 0.0742;
-    Money rent = 30000;
-	Money monthly_payment = 226312;
-	Money salary = 300000;
-	Money alice_credit = 11687000;
-	Money apartment_price = 13000000;
-	int month_counter = 1;
-	int end_loop = 0;
-    struct Depasit{
-        Money account;
-        double interest;
-    };
-    struct Depasit alice_depasite;
-        alice_depasite.account = 0;
-        alice_depasite.interest = 0,19;
-    struct Depasit bob_depasite;
-        bob_depasite.account = 1313000;
-        bob_depasite.interest = 0,19;
-	while (end_loop != 1){
-		month_counter ++;
-		if (month_counter == 13) {
-			expenditure += expenditure * inflation;
-			rent += rent * inflation;
-			salary += salary * inflation;
-			apartment_price += apartment_price * inflation;
-		};
-		alice_depasite.account += alice_depasite.account * alice_depasite.interest / 12;
-		bob_depasite.account += bob_depasite.account * bob_depasite.interest / 12;
-		alice_depasite.account += salary - expenditure - monthly_payment;
-		alice_credit -= monthly_payment;
-		bob_depasite.account += salary - expenditure - rent;
-		if (bob_depasite.account >= apartment_price){
-			printf("Боб купил квартиру быстрее.\nСчёт Боба: %lld.\nСчёт Алисы: %lld.\n", bob_depasite.account, alice_depasite.account);
-			end_loop = 1;
-		};
-		if (alice_credit <= 0){
-			printf("Алиса купила квартиру быстрее.\nСчёт Алисы: %lld.\nСчёт Боба: %lld.", alice_depasite.account, bob_depasite.account);
-			end_loop = 1;
-		};
+
+typedef long long int Money;
+struct Depasit{
+    Money account;
+    double interest;
+};
+
+Money expenditure = 399 + 22500 + 5000 + 4500;
+long double inflation = 0.0742;
+Money rent = 30000;
+Money monthly_payment = 226312;
+Money salary = 300000;
+Money alice_credit = 11687000;
+Money apartment_price = 13000000;
+int month_counter = 1;
+struct Depasit alice_depasite;
+struct Depasit bob_depasite;
+
+void alice_init(){
+	alice_depasite.account = 0;
+	alice_depasite.interest = 0,19;
+}
+
+void bob_init(){
+	bob_depasite.account = 1313000;
+	bob_depasite.interest = 0,19;
+}
+
+void alice_expense_counting(){
+	alice_depasite.account += alice_depasite.account * alice_depasite.interest / 12;
+	alice_depasite.account += salary - expenditure - monthly_payment;
+	alice_credit -= monthly_payment;
+};
+
+void bob_expense_counting(){
+	bob_depasite.account += bob_depasite.account * bob_depasite.interest / 12;
+	bob_depasite.account += salary - expenditure - rent;
+};
+
+void print(){
+    if (bob_depasite.account >= apartment_price){
+		printf("Боб купил квартиру быстрее.\nСчёт Боба: %lld.\nСчёт Алисы: %lld.\n", bob_depasite.account, alice_depasite.account);
 	};
+	if (alice_credit <= 0){
+		printf("Алиса купила квартиру быстрее.\nСчёт Алисы: %lld.\nСчёт Боба: %lld.\n", alice_depasite.account, bob_depasite.account);
+	};
+};
+
+void inflation_changes(){
+    month_counter ++;
+	if (month_counter == 13) {
+		expenditure += expenditure * inflation;
+		rent += rent * inflation;
+		salary += salary * inflation;
+		apartment_price += apartment_price * inflation;
+        month_counter = 1;
+	};
+};
+
+int main(){
+	while (bob_depasite.account < apartment_price && alice_credit > 0){
+
+		alice_init();		
+
+		bob_init();		
+
+        alice_expense_counting();
+
+        bob_expense_counting();
+
+        inflation_changes();
+
+		print();
+	};
+    return 1;
 }
