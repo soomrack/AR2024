4ea502a2c8570dd098f8f46adfff7c2346558a5f
Daniil Martyshev <87023973+cmetamka@users.noreply.github.com>
2024-09-23 19:12:05 +0300

    Update task1S.c

--- MartyshevDE/task1S.c
+++ MartyshevDE/task1S.c
@@ -1,1 +1,50 @@
+#include <stdio.h>
+
+typedef long long int money;
+
+// Структура ипотеки
+struct Mortgage
+{
+    double rate;
+    money credit;
+    money pay;
+    money month_pay;
+};
+
+// Структура человека
+struct Individual
+{
+    money zarplata;
+    money rashod;
+    money status;
+    money kvartira;
+    struct Mortgage mortgage;
+
+};
+
+// Персонажи
+struct Individual Alice;
+struct Individual Bob;  
+
+//ALICE
+void Alice_money()
+{
+    Alice.zarplata = 2e5;
+    Alice.mortgage.rate = 0.17;
+    Alice.rashod = 5e4;
+
+}
+    //Кредит Алисы (ежемесячный платеж)
+void Alice_mortgage()
+{
+    Alice.status -= Alice.mortgage.month_pay
+}
+
+//Bob
+void Bob_money()
+{
+    Bob.zarplata = 2e5;
+    Bob.rashod = 5e4;
+    Bob.kvartira = 3e4;
+}
 
