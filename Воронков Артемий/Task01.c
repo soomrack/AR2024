9a0529f1477ad2836af37bc6b3a65c14245abaa9
Karasevicheff <voronkov.artemij@gmail.com>
2024-09-24 20:49:40 +0300

    Add files via upload

--- Воронков Артемий/Task01.c
+++ Воронков Артемий/Task01.c
@@ -2,13 +2,13 @@
 #include <locale.h>
 
 
-
-    typedef long long int Money;
+    typedef long long int Money;//RUB
     typedef int Time;
-    typedef int Percent;
-
-Money deposit = 1000 * 1000;
+    Time year = 2024;
+    Time month = 9;
+    Money deposite = 1000000;
     struct Person{
+
          Money food;
          Money clothes;
          Money other_expenses;
@@ -17,79 +17,144 @@ Money deposit = 1000 * 1000;
          Money account;
 
     };
+
     struct Person Bob =
     {
     .food = 20 * 1000 ,
-     .clothes = 20 * 1000 ,
-      .other_expenses = 30 * 1000 ,
-      .payment = 30 * 1000,
-       .salary = 300 * 1000 ,
-       .account = 0
+    .clothes = 20 * 1000 ,
+    .other_expenses = 30 * 1000 ,
+    .payment = 30 * 1000,
+    .salary = 300 * 1000 ,
+    .account = 0
 
        };
-       struct Person Alice =
+
+    struct Person Alice =
     {
     .food = 20 * 1000 ,
-     .clothes = 20 * 1000 ,
-     .other_expenses = 30 * 1000,
-      .payment = 186 * 1000 ,
-        .salary = 300 * 1000,
-         .account = 0
+    .clothes = 20 * 1000 ,
+    .other_expenses = 30 * 1000,
+    .payment = 186 * 1000 ,
+    .salary = 300 * 1000,
+    .account = 0
        };
-void count_Alice()
+
+
+    void simulation()
+       {
+        year = 2024;
+        month = 9;
+        while (year != 2054 || month != 9)
+        {
+            month++;
+            count_Alice();
+            count_Bob();
+            if(month % 12 == 0){
+                    Alice_salary(year, month);
+                    Alice_clothes(year, month);
+                    Alice_food(year, month);
+                    Alice_other_expenses(year, month);
+
+
+                    Bob_salary(year, month);
+                    Bob_clothes(year, month);
+                    Bob_food(year, month);
+                    Bob_other_expenses(year, month);
+                    //printf("%lld %lld %lld\n ",Alice.account, Bob.account, year); // DEBUG
+            }
+            if(month % 12 == 0){
+                    year ++;
+                    month = 1;
+                }
+            }
+       }
+    void count_Alice()
 {
-    Time year = 2024;
-    Time month = 9;
-  for (year=2024; year <= 2054; month++) {
-    Alice.account = Alice.account + Alice.salary - Alice.clothes - Alice.food - Alice.other_expenses - Alice.payment;
-    if(month % 12 == 0){
-           // printf("%lld  %lld\n ",Alice.account, year);
-        month = 1;
-        year++;
-        Alice.salary *= 1.2;
-        Alice.clothes *= 1.2;
-        Alice.food *= 1.2;
-        Alice.other_expenses *= 1.2;
-        }
-    }
+    Alice.account =
+    Alice.account + Alice.salary - Alice.clothes -
+    Alice.food - Alice.other_expenses - Alice.payment;
 }
 
-void count_Bob()
+
+    void Alice_salary(Time year, Time month)
 {
-    Time year = 2024;
-    Time month = 9;
-
-    for (year=2024; year <= 2054; month++) {
-
-    Bob.account += Bob.salary - Bob.clothes - Bob.food - Bob.other_expenses - Bob.payment;
-    if(month % 12 == 0){
-        deposit *= (1.23 - 0.2); // printf("%lld  %lld\n ",Bob.account, year);
-        month =1;
-        year++;
-        Bob.salary *= 1.2;
-        Bob.clothes *= 1.2;
-        Bob.food *= 1.2;
-        Bob.other_expenses *= 1.2;
-       // printf("%lld \n" , deposit );
-        }
-    }
+    Alice.salary *= 1.2;
 }
-void print()
+
+
+    void Alice_clothes(Time year, Time month)
+{
+    Alice.clothes *= 1.2;
+}
+
+
+    void Alice_food(Time year, Time month)
+{
+    Alice.food *= 1.2;
+}
+
+
+    void Alice_other_expenses(Time year, Time month)
+{
+    Alice.other_expenses *= 1.2;
+}
+
+
+    void count_Bob()
+{
+
+    Bob.account =
+    Bob.account + Bob.salary - Bob.clothes -
+    Bob.food - Bob.other_expenses - Bob.payment;
+}
+
+
+    void Bob_salary(Time year, Time month)
+{
+    Bob.salary *= 1.2;
+}
+
+
+    void Bob_clothes(Time year, Time month)
+{
+    Bob.clothes *= 1.2;
+}
+
+
+    void Bob_food(Time year, Time month)
+{
+    Bob.food *= 1.2;
+}
+
+
+    void Bob_other_expenses(Time year, Time month)
+{
+    Bob.other_expenses *= 1.2;
+}
+
+
+    void Bob_deposite()
+{
+    deposite *= 1.05;
+}
+
+
+    void print()
 {
     setlocale(LC_ALL, "Russian");
-    if(Alice.account + 14000000 > Bob.account)
+    if(Alice.account + 14000000 > Bob.account + deposite)
         {
-    printf("%lld al на столько больше денег у Alice \n ",(Alice.account + 14000000) - (Bob.account + deposit) );
+    printf("%lld  на столько больше денег у Alice \n ",(Alice.account + 14000000) - (Bob.account + deposite));
         }
+
     else
         {
-         printf("%lld  на столько больше денег у Bob \n ",(Bob.account + deposit ) - (Alice.account + 14000000) );
+         printf("%lld  на столько больше денег у Bob \n ",(Bob.account + deposite) - (Alice.account + 14000000) );
     }
 }
     int main()
     {
-    count_Alice();
-    count_Bob();
+    simulation();
     print();
 return 0;
    }
