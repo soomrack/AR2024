cd2b27d45ea2cafc9897a523e489d01c2ac8077d
Agent112200393 <kolyagoryachun@gmail.com>
2024-09-30 10:41:52 +0300

    30.09.2024

--- Горячун Николай Дмитриевич/Task 1/task1.c
+++ Горячун Николай Дмитриевич/Task 1/task1.c
@@ -5,65 +5,94 @@ struct Depasit{
     Money account;
     double interest;
 };
+struct Person{
+    Money expenditure;
+    Money salary;
+    Money rent;
+    Money credit;
+    Money monthly_payment;
+};
 
-Money expenditure = 399 + 22500 + 5000 + 4500;
+
 long double inflation = 0.0742;
-Money rent = 30000;
 Money monthly_payment = 226312;
-Money salary = 300000;
-Money alice_credit = 11687000;
 Money apartment_price = 13000000;
 int month_counter = 1;
 struct Depasit alice_depasite;
 struct Depasit bob_depasite;
+struct Person alice;
+struct Person bob;
 
-void alice_init(){
+
+void alice_init()
+{
 	alice_depasite.account = 0;
-	alice_depasite.interest = 0,19;
+	alice_depasite.interest = 0.19;
+    alice.expenditure = 399 + 22500 + 5000 + 4500;
+    alice.salary = 300000;
+    alice.credit = 11687000;
+    alice.monthly_payment = 226312;
 }
 
-void bob_init(){
+
+void bob_init()
+{
 	bob_depasite.account = 1313000;
-	bob_depasite.interest = 0,19;
+	bob_depasite.interest = 0.19;
+    bob.expenditure = 399 + 22500 + 5000 + 4500;
+    bob.salary = 300000;
+    bob.rent = 30000;
 }
 
-void alice_expense_counting(){
+
+void alice_expense_counting()
+{
 	alice_depasite.account += alice_depasite.account * alice_depasite.interest / 12;
-	alice_depasite.account += salary - expenditure - monthly_payment;
-	alice_credit -= monthly_payment;
+	alice_depasite.account += alice.salary - alice.expenditure - alice.monthly_payment;
+	alice.credit -= alice.monthly_payment;
 };
 
-void bob_expense_counting(){
+
+void bob_expense_counting()
+{
 	bob_depasite.account += bob_depasite.account * bob_depasite.interest / 12;
-	bob_depasite.account += salary - expenditure - rent;
+	bob_depasite.account += bob.salary - bob.expenditure - bob.rent;
 };
 
-void print(){
+
+void print()
+{
     if (bob_depasite.account >= apartment_price){
 		printf("Боб купил квартиру быстрее.\nСчёт Боба: %lld.\nСчёт Алисы: %lld.\n", bob_depasite.account, alice_depasite.account);
 	};
-	if (alice_credit <= 0){
+	if (alice.credit <= 0){
 		printf("Алиса купила квартиру быстрее.\nСчёт Алисы: %lld.\nСчёт Боба: %lld.\n", alice_depasite.account, bob_depasite.account);
 	};
 };
 
-void inflation_changes(){
-    month_counter ++;
+
+void inflation_changes()
+{
+    month_counter++;
 	if (month_counter == 13) {
-		expenditure += expenditure * inflation;
-		rent += rent * inflation;
-		salary += salary * inflation;
+		alice.expenditure += alice.expenditure * inflation;
+		alice.salary += alice.salary * inflation;
+		bob.expenditure += bob.expenditure * inflation;
+		bob.rent += bob.rent * inflation;
+		bob.salary += bob.salary * inflation;
 		apartment_price += apartment_price * inflation;
         month_counter = 1;
 	};
 };
 
-int main(){
-	while (bob_depasite.account < apartment_price && alice_credit > 0){
 
-		alice_init();		
+int main()
+{
+    alice_init();		
 
-		bob_init();		
+	bob_init();
+
+	while (bob_depasite.account < apartment_price && alice.credit > 0){		
 
         alice_expense_counting();
 
