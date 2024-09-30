ac93e915e4f871c9dd05378fa5f74c75a4510792
Daniil Martyshev <87023973+cmetamka@users.noreply.github.com>
2024-09-30 10:38:12 +0300

    Full complete

--- MartyshevDE/task1S.c
+++ MartyshevDE/task1S.c
@@ -1,45 +1,41 @@
 #include <stdio.h>
 
-typedef long long int money;
 
-int year = 2024;
-int month = 9;
+typedef long long int Money;
 
-// Структура ипотеки
+
 struct Mortgage
 {
     double rate;
-    money credit;
-    money pay;
-    money month_pay;
+    Money credit;
+    Money pay;
+    Money month_pay;
 };
+
 
 struct Bank
 {
-    money vklad;
-    money month_pay;
-    
+    Money vklad;
+    Money month_pay;
 };
 
 
-// Структура человека
 struct Individual
 {
-    money zarplata;
-    money rashod;
-    money status;
-    money kvartira;
-    money rubles; //Оставшиеся деньги после всех расходов
+    Money zarplata;
+    Money rashod;
+    Money status;
+    Money kvartira;
+    Money rubles; 
     struct Mortgage mortgage;
     struct Bank bank;
-
 };
 
-// Персонажи
+
 struct Individual Alice;
 struct Individual Bob;  
 
-//ALICE
+
 void Alice_money()
 {
     Alice.status = 0;
@@ -50,31 +46,41 @@ void Alice_money()
     Alice.mortgage.rate = 0.17;
     Alice.mortgage.credit = 13e6;
     Alice.mortgage.month_pay = 15e4;
-
 }
-    //Кредит Алисы (ежемесячный платеж)
+
+
 void Alice_mortgage()
 {
     Alice.status -= Alice.mortgage.month_pay;
 }
 
+
 void Alice_zarplata(const int month, const int year)
 {
-    if(month == 12){
+    if(month == 12) {
         Alice.status += Alice.zarplata;
     }
-    if(month == 1){
+    if(month == 1) {
         Alice.zarplata *= 1.09;
     }
     Alice.status += Alice.zarplata;
 }
 
+
 void Alice_print()
 {
+    if (Alice.status > Bob.status) {
+        printf ("Alice has more money than Bob on %lld\n", Alice.status - Bob.status);
+    }
+
+    if (Alice.status < Bob.status) {
+        printf ("Bob has more money than Alice on %lld\n", Bob.status - Alice.status);
+    }
+
     printf ("Alice capital = %lld \n", Alice.status);
 }
 
-//BOB
+
 void Bob_money()
 {
     Bob.status = 0;
@@ -85,16 +91,18 @@ void Bob_money()
     Bob.bank.month_pay = 12e4;
 }
 
+
 void Bob_zarplata(const int month, const int year)
 {
-    if(month == 12){
+    if(month == 12) {
         Bob.status += Bob.zarplata;
     }
-    if(month == 1){
+    if(month == 1) {
         Bob.zarplata *= 1.09;
     }
     Bob.status += Bob.zarplata;
 }
+
 
 void Bob_bank(int deposit)
 {
@@ -103,27 +111,30 @@ void Bob_bank(int deposit)
 
     while( !((month == 9) && (year == 2054)) ) {
 
-    deposit = 20; //Процент за депозит
+    deposit = 20; 
     Bob.status -= Bob.bank.month_pay;
     Bob.bank.vklad = ((deposit/12)*0.01+1);
     Bob.rubles += (Bob.zarplata -Bob.bank.month_pay - Bob.rashod - Bob.kvartira);
     Bob.status += (Bob.bank.vklad + Bob.rubles);
-
     }
 }
 
 
 void Bob_kvartira(int year)
 {
-    if (year== 2034 || year == 2044 || year == 2054){
-        Bob.kvartira += 5e3;
+    year = 2024;
+    if (year== 2034 || year == 2044 || year == 2054) {
+        Bob.kvartira += 5000;
     }
 }
+
 
 void Bob_print()
 {
     printf ("Bob capital = %lld \n", Bob.status);
+
 }
+
 
 void simulation()
 {
@@ -132,11 +143,9 @@ void simulation()
 
     while( !((month == 9) && (year == 2054)) ) {
         
-        //Alice
         Alice_zarplata(month, year);
         Alice_mortgage();
         
-        //Bob
         Bob_zarplata(month, year);
         Bob_kvartira(year);
 
@@ -148,22 +157,9 @@ void simulation()
     }
 }
 
-// У кого больше денег
-void Status_Alice_and_Bob()
-{
-    if (Alice.status > Bob.status){
-        printf ("Alice has more money than Bob on %lld\n", Alice.status - Bob.status);
-    }
-
-    if (Alice.status < Bob.status){
-        printf ("Bob has more money than Alice on %lld\n", Bob.status - Alice.status);
-    }
-}
 
 int main()
 {
-    Status_Alice_and_Bob();
-
     Alice_money();
 
     Bob_money();
