7aa4616772dd22482ea03ec67f83c21eb6c27276
Agent112200393 <kolyagoryachun@gmail.com>
2024-09-30 14:19:06 +0300

    30.09.2024

--- Горячун Николай Дмитриевич/Task 1/task1.c
+++ Горячун Николай Дмитриевич/Task 1/task1.c
@@ -1,86 +1,109 @@
 #include <stdio.h>
 
-typedef long long int Money;
-struct Depasit{
+typedef long long int Money; // RUR
+
+struct Deposit {
     Money account;
     double interest;
 };
-struct Person{
-    Money expenditure;
-    Money salary;
-    Money rent;
+
+struct Credite {
     Money credit;
     Money monthly_payment;
 };
 
+struct Person {
+    Money expenditure;
+    Money salary;
+    Money apartment_price;
+    Money rent;
+    struct Deposit deposite;
+    struct Credite credit;
+};
+
 
 long double inflation = 0.0742;
-Money monthly_payment = 226312;
-Money apartment_price = 13000000;
 int month_counter = 1;
-struct Depasit alice_depasite;
-struct Depasit bob_depasite;
+
 struct Person alice;
 struct Person bob;
 
 
 void alice_init()
 {
-	alice_depasite.account = 0;
-	alice_depasite.interest = 0.19;
+	alice.deposite.account = 0;
+	alice.deposite.interest = 0.19;
     alice.expenditure = 399 + 22500 + 5000 + 4500;
     alice.salary = 300000;
-    alice.credit = 11687000;
-    alice.monthly_payment = 226312;
+    alice.credit.credit = 11687000;
+    alice.credit.monthly_payment = 226312;
 }
 
 
 void bob_init()
 {
-	bob_depasite.account = 1313000;
-	bob_depasite.interest = 0.19;
+	bob.deposite.account = 1313000;
+	bob.deposite.interest = 0.19;
     bob.expenditure = 399 + 22500 + 5000 + 4500;
     bob.salary = 300000;
     bob.rent = 30000;
+    bob.apartment_price = 13000000;
 }
 
 
 void alice_expense_counting()
 {
-	alice_depasite.account += alice_depasite.account * alice_depasite.interest / 12;
-	alice_depasite.account += alice.salary - alice.expenditure - alice.monthly_payment;
-	alice.credit -= alice.monthly_payment;
+	alice.deposite.account += alice.deposite.account * alice.deposite.interest / 12;
+	alice.deposite.account += alice.salary - alice.expenditure - alice.credit.monthly_payment;
+};
+
+void alice_credite_counting()
+{
+	alice.credit.credit -= alice.credit.monthly_payment;
 };
 
 
 void bob_expense_counting()
 {
-	bob_depasite.account += bob_depasite.account * bob_depasite.interest / 12;
-	bob_depasite.account += bob.salary - bob.expenditure - bob.rent;
+	bob.deposite.account += bob.deposite.account * bob.deposite.interest / 12;
+	bob.deposite.account += bob.salary - bob.expenditure - bob.rent;
 };
 
 
 void print()
 {
-    if (bob_depasite.account >= apartment_price){
-		printf("Боб купил квартиру быстрее.\nСчёт Боба: %lld.\nСчёт Алисы: %lld.\n", bob_depasite.account, alice_depasite.account);
+    if (bob.deposite.account >= bob.apartment_price){
+		printf("Боб купил квартиру быстрее.\nСчёт Боба: %lld.\nСчёт Алисы: %lld.\n", bob.deposite.account, alice.deposite.account);
 	};
-	if (alice.credit <= 0){
-		printf("Алиса купила квартиру быстрее.\nСчёт Алисы: %lld.\nСчёт Боба: %lld.\n", alice_depasite.account, bob_depasite.account);
+	if (alice.credit.credit <= 0){
+		printf("Алиса купила квартиру быстрее.\nСчёт Алисы: %lld.\nСчёт Боба: %lld.\n", alice.deposite.account, bob.deposite.account);
 	};
 };
+
+
+void alice_inflation()
+{
+    alice.expenditure += alice.expenditure * inflation;
+    alice.salary += alice.salary * inflation;
+};
+
+
+void bob_inflation()
+{
+    bob.expenditure += bob.expenditure * inflation;
+    bob.rent += bob.rent * inflation;
+    bob.salary += bob.salary * inflation;
+    bob.apartment_price += bob.apartment_price * inflation;
+};
+
 
 
 void inflation_changes()
 {
     month_counter++;
 	if (month_counter == 13) {
-		alice.expenditure += alice.expenditure * inflation;
-		alice.salary += alice.salary * inflation;
-		bob.expenditure += bob.expenditure * inflation;
-		bob.rent += bob.rent * inflation;
-		bob.salary += bob.salary * inflation;
-		apartment_price += apartment_price * inflation;
+        alice_inflation();
+        bob_inflation();
         month_counter = 1;
 	};
 };
@@ -92,9 +115,11 @@ int main()
 
 	bob_init();
 
-	while (bob_depasite.account < apartment_price && alice.credit > 0){		
+	while (bob.deposite.account < bob.apartment_price && alice.credit.credit > 0) {		
 
         alice_expense_counting();
+
+        alice_credite_counting();
 
         bob_expense_counting();
 
