4eb1774200a0aefce338e71a9258b1de257f5428
crazytrain11 <iusachev5@gmail.com>
2024-10-01 19:32:06 +0300

    Updated Polytech.t1.3.c

--- Усачев Иван/Polytech.t1.3.c
+++ Усачев Иван/Polytech.t1.3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 typedef long long Money;
+
 int month = 9;
 int year = 2024;
 
@@ -15,7 +16,7 @@ struct Person {
 };
 
 struct Person Alice = {300 * 1000, 100 * 1000, 0, 180 * 1000, 0, 14000 * 1000, 400 * 100};
-struct Person Bob = {300 * 1000, 100 * 1000, 0, 180 * 1000, 0, 14000 * 1000, 400 * 100};
+struct Person Bob = {0, 100 * 1000, 0, 180 * 1000, 0, 14000 * 1000, 400 * 100};
 
 void bob_salary(const int month, const int year) {
     if (month == 12) {
@@ -23,6 +24,7 @@ void bob_salary(const int month, const int year) {
     }
     Bob.account += Bob.salary;
 }
+
 
 void bob_spendings(const int month, const int year) {
     if (month == 12) {
@@ -33,7 +35,9 @@ void bob_spendings(const int month, const int year) {
     Bob.account -= Bob.spendings;
 }
 
+
 void alice_salary(const int month, const int year) {
+    static Money better_salary;
     if (month == 12) {
         Alice.salary *= 1.07;
         Alice.house *= 1.08;
@@ -42,7 +46,21 @@ void alice_salary(const int month, const int year) {
     if (month == 9 && year == 2054) {
         Alice.account += Alice.house;
     }
+    if (better_salary == 0) {
+        better_salary = Alice.salary * 1.5;
+    }
+    if (year == 2028 && month == 3) {
+        Alice.salary = 0;
+    }
+    if (year == 2028 && month == 4) {
+        Alice.salary = 0;
+    }
+    if (year == 2028 && month == 5) {
+        Alice.salary = better_salary;
+    }
+    printf("\n%lld", Alice.salary);
 }
+
 
 void alice_spendings(const int month, const int year) {
     if (month == 12) {
@@ -51,6 +69,7 @@ void alice_spendings(const int month, const int year) {
     Alice.spendings = Alice.money_for_living + Alice.mortgage;
     Alice.account -= Alice.spendings;
 }
+
 
 void simulation() {
     while (!(year == 2054 && month == 10)) {
@@ -66,6 +85,7 @@ void simulation() {
     }
 }
 
+
 void result() {
     if (Alice.account > Bob.account) {
         printf("\nAlice is richer on: %lld", Alice.account - Bob.account);
@@ -73,6 +93,7 @@ void result() {
         printf("\nBob is richer on: %lld", Bob.account - Alice.account);
     }
 }
+
 
 int main() {
     simulation();
