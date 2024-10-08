4699231221345b8d34c4c1da3ce787b5b3b9bba5
Pleshenko Roman Maximovich <kontinent16363.ru@gmail.com>
2024-10-08 13:06:46 +0300

    Update zadacha_1.c

--- Roman Pleshchenko/zadacha_1.c
+++ Roman Pleshchenko/zadacha_1.c
@@ -1,54 +1,116 @@
-#include <stdio.h> 
-int main(){
-    float A_house=14000000.0;
-    float A_capital = 0;
-    float B_bank = 1000000.0;
-    float B_capital = 0;
-    float A_credit=185337.79; // ануитетный платеж 
-    float A_savemoney = 0; //Сумма, которую может отложить Alice после всех трат
-    float B_savemoney = 0; // Сумма, которую может отложить Bob после всех трат 
+#include <stdio.h>
+typedef long long int Money;
+struct Person  {
+    Money house;
+    Money capital;
+    Money bank_deposit;
+    Money credit;
+    Money salary;
+    Money expenses;
+    Money bank;
+};
+struct Person Alice =
+    {
+    .capital = 14* 1000 * 1000,
+    .credit = 185337,
+    .salary = 300 * 1000,
+    .expenses = 60 * 1000
+};
 
-    float A_zp = 300000.0;
-    float B_zp=300000.0;
-    float A_costs = 60000.0;
-    float B_costs = 85000.0;
-    int year = 2024;
-    int month = 9 ;
-    float infl = 9.0;
-    float dep = 20.0;
-    while( year != 2054) {
-        month ++;
-        if (year == 2054 && month == 10){
-            break;
-        }
-        if (month == 13){
-            month = 1; 
-            year ++;
-            A_zp= A_zp*(1+0.01*infl);
-            B_zp= B_zp*(1+0.01*infl);
-            A_costs= A_costs*(1+0.01*infl);
-            B_costs= B_costs*(1+0.01*infl);
-            A_house = A_house*(1+0.01*infl);
-        }
-        //Аlice
-        A_savemoney = A_savemoney + (A_zp - A_credit - A_costs);
-        A_capital = A_house + A_savemoney;
-        // Bob
-        B_savemoney =B_savemoney + (B_zp - B_costs);
-        B_bank= B_bank * ((dep/12)*0.01+1);
-        B_capital = B_bank + B_savemoney;
+struct Person Bob =
+    {
+    .capital = 1000*1000,
+    .salary = 300*1000,
+    .expenses = 95 * 1000,
+    .bank = 1000*1000
+};
+
+void alice_credit() {
+    Alice.capital = Alice.capital - Alice.credit;
+}
+
+
+void alice_expenses() {
+    Alice.capital= Alice.capital - Alice.expenses;
+}
+
+
+void alice_salary() {
+    Alice.capital = Alice.capital + Alice.salary;
+}
+
+
+void bob_deposit() {
+
+    Bob.capital = Bob.capital + Bob.bank*((20/12) * 0.01);
+    Bob.bank = Bob.bank * ((20/12) * 0.01+1);
+}
+
+
+void bob_expenses() {
+    Bob.capital= Bob.capital - Bob.expenses;
+}
+
+
+void bob_salary() {
+    Bob.capital = Bob.capital + Bob.salary;
+}
+
+
+void infl()
+{
+    Alice.expenses = Alice.expenses * 1.07;
+    Bob.expenses = Bob.expenses * 1.07;
+    Alice.salary = Alice.salary * 1.07;
+    Bob.salary = Bob.salary * 1,07;
+}
+
+void print() {
+    if (Bob.capital>Alice.capital){
+        printf("Bob richer  = %lld \n ", Bob.capital - Alice.capital);
     }
-    if (B_capital>A_capital){
-        printf("Bob richer  = %f \n ", B_capital - A_capital);
+    if (Bob.capital < Alice.capital){
+        printf("Alice richer =  %lld \n ", Alice.capital - Bob.capital);
     }
-    if (B_capital < A_capital){
-        printf("Alice richer =  %f \n ", A_capital - B_capital);
-    }
-    if (B_capital == A_capital){
+    if (Bob.capital == Alice.capital){
         printf("Bob and Alice have the same capital");
     }
-    printf("Bob's capital = %f \n", B_capital);
-    printf("Alice's capital = %f \n", A_capital);
+}
+
+void simulation()
+{
+    int month = 9;
+    int year = 2024;
+    while ( !((month == 10) && (year == 2054)) ) {
+        bob_salary();
+        alice_salary();
+        bob_expenses();
+        alice_expenses();
+        alice_credit();
+        bob_deposit();
+
+        month++;
+        if (month == 13) {
+            month = 1;
+            year++;
+            //infl();
+        }
+
+
+
+
+
+
+    }
+}
+
+
+int main() {
+    simulation();
+    print();
     return 0;
-    
+
+
+
+
 }
