fe61e638ddfd5471c6e05380842063851e47e23f
Agent112200393 <kolyagoryachun@gmail.com>
2024-10-14 00:09:01 +0300

    14.10.2024

--- Горячун Николай Дмитриевич/Task 1/task1.c
+++ Горячун Николай Дмитриевич/Task 1/task1.c
@@ -2,28 +2,30 @@
 
 typedef long long int Money; // RUR
 
+
 struct Deposit {
     Money account;
     double interest;
 };
 
-struct Credite {
-    Money credit;
-    Money monthly_payment;
-};
 
 struct Person {
     Money expenditure;
     Money salary;
     Money apartment_price;
     Money rent;
+    Money credit_monthly_payment;
+    int year_of_raising;
+    Money car_price;
+	int buy_car;
     struct Deposit deposite;
-    struct Credite credit;
 };
 
 
 long double inflation = 0.0742;
 int month_counter = 1;
+int year_counter = 0;
+int new_year = 0;
 
 struct Person alice;
 struct Person bob;
@@ -33,10 +35,9 @@ void alice_init()
 {
 	alice.deposite.account = 0;
 	alice.deposite.interest = 0.19;
-    alice.expenditure = 399 + 22500 + 5000 + 4500;
+    alice.expenditure = 399 + 22500 + 5000 + 4500; // Расходы за месяй (Цена подписки СберПрайм + еда + одежда + комуналка)
     alice.salary = 300000;
-    alice.credit.credit = 11687000;
-    alice.credit.monthly_payment = 226312;
+    alice.credit_monthly_payment = 226312;
 }
 
 
@@ -44,22 +45,20 @@ void bob_init()
 {
 	bob.deposite.account = 1313000;
 	bob.deposite.interest = 0.19;
-    bob.expenditure = 399 + 22500 + 5000 + 4500;
+    bob.expenditure = 399 + 22500 + 5000 + 4500; // Расходы за месяй (Цена подписки СберПрайм + еда + одежда + комуналка)
     bob.salary = 300000;
     bob.rent = 30000;
     bob.apartment_price = 13000000;
+    bob.year_of_raising = 3;
+    bob.car_price = 4000000;
+    bob.buy_car = 0;
 }
 
 
 void alice_expense_counting()
 {
 	alice.deposite.account += alice.deposite.account * alice.deposite.interest / 12;
-	alice.deposite.account += alice.salary - alice.expenditure - alice.credit.monthly_payment;
-};
-
-void alice_credite_counting()
-{
-	alice.credit.credit -= alice.credit.monthly_payment;
+	alice.deposite.account += alice.salary - alice.expenditure - alice.credit_monthly_payment;
 };
 
 
@@ -67,17 +66,6 @@ void bob_expense_counting()
 {
 	bob.deposite.account += bob.deposite.account * bob.deposite.interest / 12;
 	bob.deposite.account += bob.salary - bob.expenditure - bob.rent;
-};
-
-
-void print()
-{
-    if (bob.deposite.account >= bob.apartment_price){
-		printf("Боб купил квартиру быстрее.\nСчёт Боба: %lld.\nСчёт Алисы: %lld.\n", bob.deposite.account, alice.deposite.account);
-	};
-	if (alice.credit.credit <= 0){
-		printf("Алиса купила квартиру быстрее.\nСчёт Алисы: %lld.\nСчёт Боба: %lld.\n", alice.deposite.account, bob.deposite.account);
-	};
 };
 
 
@@ -94,18 +82,67 @@ void bob_inflation()
     bob.rent += bob.rent * inflation;
     bob.salary += bob.salary * inflation;
     bob.apartment_price += bob.apartment_price * inflation;
+    bob.car_price += bob.car_price * inflation;
 };
 
 
 
 void inflation_changes()
 {
-    month_counter++;
-	if (month_counter == 13) {
+	if (new_year == 1) {
         alice_inflation();
         bob_inflation();
+        new_year = 0;
+    }
+};
+
+
+void month_and_year_counter()
+{
+    month_counter++;
+	if (month_counter == 13) {
+		year_counter++;
         month_counter = 1;
+		new_year = 1;
 	};
+};
+
+
+void bob_raising()
+{
+    if (year_counter == bob.year_of_raising) {
+        bob.salary += 50000;
+    };
+};
+
+
+void bob_buy_car()
+{
+    if (bob.buy_car == 0 && bob.deposite.account >= bob.car_price) {
+        bob.deposite.account -= bob.car_price;
+		bob.buy_car = 1;
+    };
+};
+
+
+void cycle_body()
+{
+    bob_raising();
+    alice_expense_counting();
+    bob_expense_counting();
+    bob_buy_car();
+    month_and_year_counter();
+    inflation_changes();
+};
+
+
+void print()
+{
+    if (bob.buy_car == 1) {
+        printf("Боб купил машину.\nЦена квартиры: %lld.\nСчёт Боба: %lld.\nСчёт Алисы: %lld.\n", bob.apartment_price, bob.deposite.account, alice.deposite.account);
+    } else {
+        printf("Боб не купил машину.\nЦена квартиры: %lld.\nСчёт Боба: %lld.\nСчёт Алисы: %lld.\n", bob.apartment_price, bob.deposite.account, alice.deposite.account);
+    };
 };
 
 
@@ -115,17 +152,9 @@ int main()
 
 	bob_init();
 
-	while (bob.deposite.account < bob.apartment_price && alice.credit.credit > 0) {		
-
-        alice_expense_counting();
-
-        alice_credite_counting();
-
-        bob_expense_counting();
-
-        inflation_changes();
-
-		print();
+	while (year_counter != 30) {		
+        cycle_body();
 	};
-    return 1;
+    print();
+    return 0;
 }
