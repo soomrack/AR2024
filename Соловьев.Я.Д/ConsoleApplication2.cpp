39511b4df95b5d4eff8ecdddbbf5cba48f11af97
yaroslav-soloviev <aroslav6789@gmail.com>
2024-10-14 21:25:32 +0300

    Update ConsoleApplication2.cpp

--- Соловьев.Я.Д/ConsoleApplication2.cpp
+++ Соловьев.Я.Д/ConsoleApplication2.cpp
@@ -4,7 +4,6 @@
 typedef long long int Money;
 
 struct Mortgage {
-	double rate=0.17;
 	Money credit;
 	Money platez;
 	Money month_pay;
@@ -15,7 +14,7 @@ struct Mortgage {
 
 struct Person {
 	Money salary;
-	Money ostatok;
+	Money account;
 	Money expences;
 	struct Mortgage mortgage;
 	Money flat_cost;
@@ -24,95 +23,98 @@ struct Person {
 	Money profit;
 	Money profit_savings;
 	Money all_money;
-	
+	Money deposit_account = 1000000;
+
 };
 
 struct Person alice;  // mortgage
 struct Person bob;
 
 void home_month_pay() {
+	double rate = 0.17;
 	alice.mortgage.year = 30;
 	alice.flat_cost = 14000000;
-	 alice.mortgage.payment = 1000000;
-	 alice.mortgage.month_pay = (alice.flat_cost - alice.mortgage.payment) * alice.mortgage.rate / 12 / (1 - pow(1 + alice.mortgage.rate / 12, -alice.mortgage.year * alice.mortgage.month));
+	alice.mortgage.payment = 1000000;
+	alice.mortgage.month_pay = (alice.flat_cost - alice.mortgage.payment) * rate / 12 / (1 - pow(1 + rate / 12, -alice.mortgage.year * alice.mortgage.month));
 }
-void alice_ostatok() {
-	 alice.salary = 300000;
-	 alice.expences = 50000;
-	alice.ostatok += alice.salary - alice.expences - alice.mortgage.month_pay;
+
+
+void alice_account_month() {
+	alice.salary = 300000;
+	alice.expences = 50000;
+	alice.account += alice.salary - alice.expences - alice.mortgage.month_pay;
 }
-void bob_ostatok_month() {
+
+
+void bob_account_month() {
 	bob.salary = 250000;
 	bob.expences = 50000;
-	bob.ostatok += (bob.salary - bob.expences);
+	bob.account += (bob.salary - bob.expences);
 
 }
+
+
 void bob_savings_bank() {
-	bob.bank_rate = 0, 16;
-	bob.ostatok = bob.profit + bob.ostatok;
-	bob.profit = bob.ostatok * bob.bank_rate;
-	
-}
-void bob_first_capitall_bank() {
-	bob.savings = 1000000;
-	bob.profit_savings += bob.savings * bob.bank_rate;
-	bob.savings += bob.savings * bob.bank_rate;
-	
+	double bank_rate = 0.16;
+	bob.deposit_account *= bank_rate;
+	bob.deposit_account += bob.account;
 
 }
+
+
 
 void simulation() {
 	int month = 9;
 	int year = 2024;
 	while (!((month == 9) && (year == 2024 + 30))) {
-		bob_ostatok_month();
-		bob_first_capitall_bank();
+		bob_account_month();
 		bob_savings_bank();
 		home_month_pay();
-		alice_ostatok();
+		alice_account_month();
+
 		month++;
-
-
 		if (month == 13) {
 			month = 1;
 			year++;
 		}
 	}
 }
-	 
-	void bob_all_money() {
-		bob.all_money = bob.ostatok + bob.savings + bob.savings;
-}
-	void alice_all_money() {
-		alice.all_money = alice.flat_cost + alice.ostatok;
-	}
 
- 
-void  end()
+
+void bob_all_money() {
+	bob.all_money = bob.deposit_account;
+}
+
+
+void alice_all_money() {
+	alice.all_money = alice.flat_cost + alice.account;
+}
+
+
+void  print_result()
 {
 	if (bob.all_money > alice.all_money)
 	{
-		printf("Bob richer Alice");
+		printf("Bob richer than Alice");
 	}
-	else 
+	else
 	{
-		printf("Alice richer bob");
+		printf("Alice richer than bob");
 
 	}
 
 }
 
- int main() {
-	 home_month_pay();
-	 alice_ostatok();
-	 bob_ostatok_month();
-	 bob_savings_bank();
-	 bob_first_capitall_bank();
-	 simulation();
-	 bob_all_money();
-	 alice_all_money();
-	 end();
-	 
+int main() {
+	home_month_pay();
+	alice_account_month();
+	bob_account_month();
+	bob_savings_bank();
+	simulation();
+	bob_all_money();
+	alice_all_money();
+	print_result();
+
 }
 
 
