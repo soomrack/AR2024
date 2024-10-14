b28c41c740fd624e4747b42b76a1872d86ae647d
Ксения Очкасова <oksenia524@gmail.com>
2024-10-14 10:18:43 +0300

    Update task2.c

--- Ochkasova Ksenia/task2.c
+++ Ochkasova Ksenia/task2.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+struct Matrix {
+    size_t cols;
+    size_t rows;
+    double* date;
+}
+
+struct Matrix Matrix_allocate (size_t cols, size_t rows) {
+    data = malloc (cols*rows*sizeof(double))
+    if (data == NULL) {
+        return (struct {0,0,null})
+    }
+}
+
+void Matrix_free () {
+    if (data != NULL) {
+        free(data);
+        data = NULL;
+    }
+}
