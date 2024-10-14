4b32b9a477d9c72f07e306884327ab4c762084a8
MichaelDolgopolov <Misha36km@gmail.com>
2024-10-14 15:22:31 +0300

    Update and rename Dolgopolov Michail/task 1 to Dolgopolov Michail/task1/task1.c

--- Dolgopolov Michail/task1/task1.c
+++ Dolgopolov Michail/task1/task1.c
@@ -3,49 +3,51 @@
 typedef long long int Money; //RUR
 
 long double inflation = 0.10;
-Money flat_cost = 10 * 1000 * 1000;
+Money alice_flat_cost = 10 * 1000 * 1000;
 
-struct Deposit
-{
+
+struct Deposit{
    Money deposit_account;
    double rate;
    Money month_pay;
 };
 
-struct Mortgage
-{
+
+struct Mortgage{
    double rate;
    Money credit;
    Money platez; 
    Money month_pay;
 };
 
-struct Person
-{
+
+struct Person{
    Money salary;
    Money account;
    Money food;
    Money expences;
+   Money rent;
+
    struct Mortgage mortgage;
    struct Deposit deposit;
-   Money rent;
 };
 
 struct Person alice;
 struct Person bob;
 
-//Alice
 void alice_init()
 {
    alice.account = 1000 * 1000;
    alice.salary = 200 * 1000;
    alice.food = 17 * 1000;
    alice.expences = 40 * 1000;
+
    alice.mortgage.rate = 0.19;
    alice.mortgage.credit = 10 * 1000 * 1000;
    alice.mortgage.platez = 1000 * 1000;
    alice.mortgage.month_pay = 143 * 1000;
    alice.account -= alice.mortgage.platez;
+
    alice.deposit.deposit_account;
    alice.deposit.rate = 0.20;
 }
@@ -53,8 +55,7 @@ void alice_init()
 void alice_expences(const int month)
 {
    alice.account -= alice.expences;
-   if (month == 1)
-   {
+   if (month == 1){
       alice.expences += alice.expences * inflation;
    }
 }
@@ -62,8 +63,7 @@ void alice_expences(const int month)
 void alice_food(const int month)
 {
    alice.account -= alice.food;
-   if (month == 1)
-   {
+   if (month == 1){
       alice.food += alice.food * inflation;
    }
 }
@@ -71,8 +71,7 @@ void alice_food(const int month)
 void alice_salary(const int month)
 {
    alice.account += alice.salary;
-   if (month == 1)
-   {
+   if (month == 1){
       alice.salary += alice.salary * inflation;
    }
 }
@@ -88,36 +87,36 @@ void alice_deposit()
    alice.deposit.deposit_account += alice.salary - alice.food - alice.expences - alice.mortgage.month_pay;
 }
 
-void flat_cost_height(const int month)
+void alice_flat_cost_height(const int month)
 {
-   if (month == 1)
-   {
-      flat_cost += flat_cost * inflation;
+   if (month == 1){
+      alice_flat_cost += alice_flat_cost * inflation;
    }
 }
 
 void alice_print()
 {
-   printf("Alice account = %lld \n", flat_cost + alice.account + alice.deposit.deposit_account);
+   printf("Alice account = %lld \n", alice_flat_cost + alice.account + alice.deposit.deposit_account);
 }
 
-//Bob
+
 void bob_init()
 {
    bob.account;
    bob.salary = 200 * 1000;
    bob.food = 17 * 1000;
    bob.expences = 40 * 1000;
+
    bob.deposit.deposit_account = 1000 * 1000;
    bob.deposit.rate = 0.20;
+
    bob.rent = 30 * 1000;
 }
 
 void bob_expences(const int month)
 {
    bob.account -= bob.expences;
-   if (month == 1)
-   {
+   if (month == 1){
       bob.expences += bob.expences * inflation;
    }
 }
@@ -125,8 +124,7 @@ void bob_expences(const int month)
 void bob_food(const int month)
 {
    bob.account -= bob.food;
-   if (month == 1)
-   {
+   if (month == 1){
       bob.food += bob.food * inflation;
    }
 }
@@ -134,8 +132,7 @@ void bob_food(const int month)
 void bob_salary(const int month)
 {
    bob.account += bob.salary;
-   if (month == 1)
-   {
+   if (month == 1){
       bob.salary += bob.salary * inflation;
    }
 }
@@ -143,8 +140,7 @@ void bob_salary(const int month)
 void bob_rent(const int month)
 {
    bob.account -= bob.rent;
-   if (month == 1)
-   {
+   if (month == 1){
       bob.rent += bob.rent * inflation;
    }
 }
@@ -163,10 +159,9 @@ void bob_print()
 void simulation()
 {
    int month = 10;
-   int yaer = 2024;
-   while (!((month == 10) && (yaer == 2024 + 30)))
-   {
-      flat_cost_height(month);
+   int year = 2024;
+   while (!((month == 10) && (year == 2024 + 30))){
+      alice_flat_cost_height(month);
       alice_salary(month);
       alice_mortgage();
       alice_food(month);
@@ -178,31 +173,31 @@ void simulation()
       bob_expences(month);
       bob_deposit();
       month++;
-      if (month == 13)
-      {
+      if (month == 13){
          month = 1;
-         yaer++;
+         year++;
       }
    }
 }
 
 void result()
 {
-   if (alice.account >= bob.account)
-   {
+   if (alice.account >= bob.account){
       printf("Alice won\n");
    }
-   else
-   {
+   else{
       printf("Bob won\n");
    }
 }
+
 
 int main()
 {
    bob_init();
    alice_init();
+   
    simulation();
+
    alice_print();
    bob_print();
    result();
