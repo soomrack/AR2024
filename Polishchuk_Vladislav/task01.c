ba14e9cb6261b11e8ea736c2aa04f5244a218d80
masterp00909 <masterpo0909@gmail.com>
2024-10-21 20:53:23 +0300

    task01 is done

--- Polishchuk_Vladislav/task01.c
+++ Polishchuk_Vladislav/task01.c
@@ -22,7 +22,6 @@ struct Person {
     Money expenses;
     Money flat;
     Money remains;
-    Money count;
     struct Mortgage mortgage;
     struct Bank bank;
 };
@@ -32,11 +31,17 @@ struct Person Alice;
 struct Person Bob;  
 
 
+void Alice_expenses()
+{
+    Alice.expenses = 50 * 1000;
+}
+
+
 void Alice_money()
 {
-    Alice.count = 0;
+    Alice.bank.vklad = 0;
     Alice.salary = 200 * 1000;
-    Alice.expenses = 50 * 1000;
+    
     Alice.remains = 0;
 
     Alice.mortgage.inter_rate = 0.20;
@@ -47,42 +52,47 @@ void Alice_money()
 
 void Alice_mortgage()
 {
-    Alice.count -= Alice.mortgage.month_pay;
+    Alice.bank.vklad -= Alice.mortgage.month_pay;
 }
 
 
 void Alice_salary(const int month)
 {
     if(month == 12) {
-        Alice.count += Alice.salary;
+        Alice.bank.vklad += Alice.salary;
     }
 
     if(month == 1) {
         Alice.salary *= 1.08;
     }
 
-    Alice.count += Alice.salary;
+    Alice.bank.vklad += Alice.salary;
 }
 
 
 void Alice_print()
 {
-    if (Alice.count > Bob.count) {
-        printf ("Alice has more money than Bob on %lld\n", Alice.count - Bob.count);
+    if (Alice.bank.vklad > Bob.bank.vklad) {
+        printf ("Alice has more money than Bob on %lld\n", Alice.bank.vklad - Bob.bank.vklad);
     }
 
-    if (Alice.count < Bob.count) {
-        printf ("Bob has more money than Alice on %lld\n", Bob.count - Alice.count);
+    if (Alice.bank.vklad < Bob.bank.vklad) {
+        printf ("Bob has more money than Alice on %lld\n", Bob.bank.vklad - Alice.bank.vklad);
     }
-    printf ("Alice capital = %lld \n", Alice.count);
+    printf ("Alice capital = %lld \n", Alice.bank.vklad);
+}
+
+
+void Bob_expenses()
+{
+    Bob.expenses = 50 * 1000;
 }
 
 
 void Bob_money()
 {
-    Bob.count = 0;
+    Bob.bank.vklad = 0;
     Bob.salary = 200 * 1000;
-    Bob.expenses = 50 * 1000;
     Bob.flat = 30 * 1000; 
     Bob.remains = 0;
     Bob.bank.invest = 120 * 1000;
@@ -92,39 +102,43 @@ void Bob_money()
 void Bob_salary(const int month)
 {
     if (month == 12) {
-        Bob.count += Bob.salary;
+        Bob.bank.vklad += Bob.salary;
     }
     if (month == 1) {
         Bob.salary *= 1.08;
     }
 
-    Bob.count += Bob.salary;
+    Bob.bank.vklad += Bob.salary;
 }
 
 
-void Bob_bank(int deposit)
+void Bob_bank(const int rate)
 {
-    Bob.bank.vklad += (Bob.count * deposit / 100);
-    Bob.bank.vklad += (Bob.bank.vklad * 0.01 / 12);
+    Bob.bank.vklad += (Bob.bank.vklad * rate / 100);
 }
 
 
 void Bob_print()
 {
-    printf ("Bob capital = %lld\n", Bob.count);
+    printf ("Bob capital = %lld\n", Bob.bank.vklad);
 }
 
 
-void Bob_car(int month, int flag)
+void Bob_car(int month)
 {   
-    if (Bob.bank.vklad >= 3 * 1000 * 1000 && flag == 1){
-        Bob.count -= 3 * 1000 * 1000;
+    static int has_car = 0;
+
+    if (Bob.bank.vklad >= 3 * 1000 * 1000 && has_car == 0){
         Bob.bank.vklad -= 3 * 1000 * 1000;
-        flag = 0;
-    } else if(flag == 0 && month != 9){
-        Bob.count -= 15 * 1000;
-    } else if(flag == 0 && month == 9){
-        Bob.count -= 40 * 1000;
+        has_car = 1;
+    } 
+    if(has_car == 1){
+        if (month == 9){
+            Bob.bank.vklad -= 15 * 1000;
+        }
+        else{
+            Bob.bank.vklad -= 40 * 1000;
+        }
     }
 }
 
@@ -140,11 +154,12 @@ void simulation()
         
         Alice_salary(month);
         Alice_mortgage();
+        Alice_expenses();
         
         Bob_salary(month);
         Bob_bank(20);
-
-        Bob_car(month, 1);
+        Bob_car(month);
+        Bob_expenses();
 
         month++;
         if (month == 13) {
