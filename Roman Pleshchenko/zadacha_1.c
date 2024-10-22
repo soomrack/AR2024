eb0acd7fb9efbdc08673446f3e1d5cfe4f4f880b
Pleshenko Roman Maximovich <kontinent16363.ru@gmail.com>
2024-10-22 13:14:38 +0300

    Update zadacha_1.c

--- Roman Pleshchenko/zadacha_1.c
+++ Roman Pleshchenko/zadacha_1.c
@@ -1,56 +1,78 @@
 #include <stdio.h>
-typedef long long int Money;
+typedef long long int Money; //Rub
+typedef int time; // сколько живет кот
+
+
 struct Person {
     Money house;
-    Money capital;
-    Money credit;
+    Money account;
+    Money month_credit;
     Money salary;
     Money expenses;
 
 };
+
+
+struct Cat {
+    Money cat_expenses;
+    time cat_life;
+
+};
+
+
 struct Deposite {
     Money deposite_account;
     double deposite_procent;
 };
+
+
 struct Person Alice = {
-    .capital = 14* 1000 * 1000,
-    .credit = 185337,
+    .account = 14 * 1000 * 1000,
+    .month_credit = 185337,
     .salary = 300 * 1000,
     .expenses = 60 * 1000
 };
 
+
+struct Cat Alice_cat = {
+    .cat_expenses = 5000,
+    .cat_life = 0
+
+
+};
 struct Person Bob = {
-    .capital = 1000*1000,
-    .salary = 300*1000,
+    .account = 0,
+    .salary = 300 * 1000,
     .expenses = 95 * 1000,
 };
 
+
 struct Deposite Bob_dep = {
     .deposite_account = 1000*1000,
-    .deposite_procent = 1.06
+    .deposite_procent = 1.016
     };
+
 
 struct Deposite Alice_dep = {
     .deposite_account = 0,
-    .deposite_procent = 1.05
+    .deposite_procent = 1.016
     };
 
-void alice_deposite(const int month)
+
+void alice_deposite()
 {
-    if (month ==1) {
-        Alice.capital = Alice.capital + Alice_dep.deposite_account * Alice_dep.deposite_procent - Alice_dep.deposite_account;
-        Alice_dep.deposite_account = Alice_dep.deposite_account * Alice_dep.deposite_procent;
-    }
+    Alice_dep.deposite_account = Alice_dep.deposite_account * Alice_dep.deposite_procent;
+    Alice_dep.deposite_account += Alice.salary - Alice.month_credit - Alice.expenses;
+    Alice.account -= Alice.salary - Alice.month_credit - Alice.expenses;
 }
 
 
-void bob_deposite(const int month)
+void bob_deposite()
 {
-    if (month ==1) {
-        Bob.capital =Bob.capital + Bob_dep.deposite_account * Bob_dep.deposite_procent - Bob_dep.deposite_account;
-        Bob_dep.deposite_account = Bob_dep.deposite_account * Bob_dep.deposite_procent;
+    Bob_dep.deposite_account = Bob_dep.deposite_account * Bob_dep.deposite_procent;
+    Bob_dep.deposite_account += Bob.salary - Bob.expenses;
+    Bob.account -= Bob.salary - Bob.expenses;
 
-    }
 }
 
 
@@ -60,8 +82,7 @@ void alice_expenses(const int month)
         Alice.expenses = Alice.expenses * 1.07;
 
     }
-    Alice.capital= Alice.capital - Alice.expenses;
-    Alice_dep.deposite_account = Alice_dep.deposite_account - Alice.expenses;
+    Alice.account -= Alice.expenses;
 }
 
 
@@ -71,20 +92,35 @@ void alice_salary(const int month)
         Alice.salary = Alice.salary * 1.07;
 
     }
-    Alice.capital = Alice.capital + Alice.salary;
-    Alice_dep.deposite_account = Alice_dep.deposite_account + Alice.salary;
+    Alice.account += Alice.salary;
 }
 
 
 void alice_credit()
 {
 
-    Alice.capital = Alice.capital - Alice.credit;
-    Alice_dep.deposite_account = Alice_dep.deposite_account - Alice.credit;
+    Alice.account -= Alice.month_credit;
+}
 
+
+void alice_house(const int month)
+{
+    if (month == 1) {
+        Alice.account += Alice.house * 0,7;
+        Alice.house *= 1.07;
+    }
 
 }
 
+
+void alice_cat(const int month) {
+    if (Alice_cat.cat_life < 15){ // когда коту исполняется 15 лет, он умирает
+        Alice.account -= Alice_cat.cat_expenses;
+        if (month == 5 ) {      //у кота в мае День Рождения
+            Alice_cat.cat_life += 1;
+        }
+    }
+}
 
 void bob_expenses(const int month)
 {
@@ -92,8 +128,7 @@ void bob_expenses(const int month)
         Bob.expenses = Bob.expenses * 1.07;
 
     }
-    Bob.capital= Bob.capital - Bob.expenses;
-    Bob_dep.deposite_account = Bob_dep.deposite_account - Bob.expenses;
+    Bob.account -= Bob.expenses;
 }
 
 
@@ -103,20 +138,19 @@ void bob_salary(const int month)
         Bob.salary = Bob.salary * 1.07;
 
     }
-    Bob.capital = Bob.capital + Bob.salary;
-    Bob_dep.deposite_account = Bob_dep.deposite_account + Bob.salary;
+    Bob.account -= Bob.salary;
 }
 
 
 void print()
 {
-    if (Bob.capital>Alice.capital){
-        printf("Bob richer  = %lld \n ", Bob.capital - Alice.capital);
+    if (Bob.account + Bob_dep.deposite_account > Alice.account + Alice_dep.deposite_account){
+        printf("Bob richer  = %lld \n ", Bob.account + Bob_dep.deposite_account - (Alice.account + Alice_dep.deposite_account));
     }
-    if (Bob.capital < Alice.capital){
-        printf("Alice richer =  %lld \n ", Alice.capital - Bob.capital);
+    if (Bob.account + Bob_dep.deposite_account < Alice.account + Alice_dep.deposite_account){
+        printf("Alice richer =  %lld \n ", Alice.account + Alice_dep.deposite_account - (Bob.account + Bob_dep.deposite_account));
     }
-    if (Bob.capital == Alice.capital){
+    if (Bob.account + Bob_dep.deposite_account == Alice.account + Alice_dep.deposite_account){
         printf("Bob and Alice have the same capital");
     }
 }
@@ -130,24 +164,22 @@ void simulation()
         //Bob
         bob_salary(month);
         bob_expenses(month);
-        bob_deposite(month);
+        bob_deposite();
+
         //Alice
         alice_salary(month);
         alice_expenses(month);
         alice_credit();
-        alice_deposite(month);
+        alice_deposite();
+        alice_house(month);
+        alice_cat(month);
 
         month++;
         if (month == 13) {
             month = 1;
             year++;
         }
-
-
-
-
-
-
+        
     }
 }
 
@@ -157,8 +189,4 @@ int main()
     simulation();
     print();
     return 0;
-
-
-
-
 }
