798b364fe78f25e7f5285250456eba841327ba36
Artemi0i <Artemzed2018@mail.ru>
2024-11-19 12:52:51 +0300

    Update tack1.2.c

--- ZhukolinAD/tack1.2.c
+++ ZhukolinAD/tack1.2.c
@@ -8,24 +8,29 @@ int month = 9;
 int year = 2024;
 
 
-struct Human {
+struct Human 
+{
   Money salary;
   Money account;
   Money house;
   Money rent;
   Money deposit;
+  Money cat;
   double deposit_procent;
 };
 
 
-struct Expenses {
+struct Expenses 
+{
   Money food;
+  Money cat;
   Money utilities;
   Money other;
 };
 
 
-struct Mortgage {
+struct Mortgage 
+{
   double procent;
   Money summa_credita;
   Money platez_month;
@@ -34,21 +39,24 @@ struct Mortgage {
 };
 
 
-struct Mortgage ALice = {
+struct Mortgage ALice = 
+{
     .procent = 1.04,
     .platez_month = 141745,
     .yers = 10
 };
 
 
-struct Expenses alice = {
+struct Expenses alice = 
+{
   .food = 30000,
   .utilities = 40000,
   .other = 30000
 };
 
 
-struct Human Alice = {
+struct Human Alice = 
+{
   .salary = 300*1000,
   .house = 14 * 1000 * 1000,
   .account = 0,
@@ -57,24 +65,29 @@ struct Human Alice = {
 };
 
 
-struct Expenses bob = {
+struct Expenses bob = 
+{
   .food = 40000,
   .utilities = 40000,
-  .other = 100000
+  .other = 100000;
 };
 
 
-struct Human Bob = {
+struct Human Bob = 
+{
   .salary=300*1000,
   .account=0,
   .rent = 40*1000,
   .deposit = 1000000,
+  .cat = 15000;
   .deposit_procent = 1.05
 };
 
 
-void alice_salary(const int month, const int year) {
-  if (month == 12) {
+void alice_salary(const int month, const int year) 
+{
+  if (month == 12) 
+  {
       Alice.salary *= 1.07;
   }
 
@@ -82,8 +95,10 @@ void alice_salary(const int month, const int year) {
 }
 
 
-void alice_expenses(const int month, const int year) {
-  if (month == 12) {
+void alice_expenses(const int month, const int year) 
+{
+  if (month == 12) 
+  {
       alice.food *= 1.07;
       alice.other *= 1.07;
       alice.utilities *= 1.07;
@@ -93,30 +108,37 @@ void alice_expenses(const int month, const int year) {
 }
 
 
-void alice_mortgage(const int month, const int year) {
+void alice_mortgage(const int month, const int year) 
+{
 	Alice.account -= ALice.platez_month;
 }; 
 
 
-void alice_deposite(const int month, const int year) {
+void alice_deposite(const int month, const int year) 
+{
   Alice.deposit += Alice.account;
   Alice.account = 0;
   Alice.deposit *= Alice.deposit_procent;
 }
 
 
-void alice_cost_house(const int month, const int year) {
-    if (month == 12) {
+void alice_cost_house(const int month, const int year) 
+{
+    if (month == 12) 
+    {
         Alice.house *= 1.07;
     }
-    if (month == 9 && year == 2024 + ALice.yers) {
+    if (month == 9 && year == 2024 + ALice.yers) 
+    {
         Alice.deposit += Alice.house;
     }
 }
 
 
-void bob_salary(const int month, const int year) {
-  if (month == 12) {
+void bob_salary(const int month, const int year) 
+{
+  if (month == 12) 
+  {
       Bob.salary *= 1.07;
   }
 
@@ -124,8 +146,10 @@ void bob_salary(const int month, const int year) {
 }
 
 
-void bob_expenses(const int month, const int year) {
-  if (month == 12) {
+void bob_expenses(const int month, const int year) 
+{
+  if (month == 12) 
+  {
       Bob.rent *= 1.07;
       bob.food *= 1.07;
       bob.other *= 1.07;
@@ -136,7 +160,22 @@ void bob_expenses(const int month, const int year) {
 }
 
 
-void bob_deposit(const int month, const int year) { 
+void bob_cat(const int month, const int year)
+{
+	if month == 12 
+		{
+			Bob.cat *= 1.07;
+			Bob.account -= Bob.cat;
+		}
+	if (month == 9 && year == 2024 + 5)
+		{
+			Bob.cat = 0;
+		}
+}
+
+
+void bob_deposit(const int month, const int year) 
+{ 
   Bob.deposit += Bob.account;
   Bob.account = 0;
   Bob.deposit *= Bob.deposit_procent;
@@ -144,20 +183,24 @@ void bob_deposit(const int month, const int year) {
 
 
 
-void simulation() {
-  while (!(year == 2024 + ALice.yers && month == 10)) {
-    alice_salary(month, year); // зп
-    alice_expenses(month, year); // расходы
-    alice_mortgage(month, year); // платёж по ипотеке
-    alice_deposite(month, year); // вклад под проценты
-    alice_cost_house(month, year); // индексация стоимости дома
+void simulation() 
+{
+  while (!(year == 2024 + ALice.yers && month == 10)) 
+  {
+    alice_salary(month, year);  // зп
+    alice_expenses(month, year);  // расходы
+    alice_mortgage(month, year);  // платёж по ипотеке
+    alice_deposite(month, year);  // вклад под проценты
+    alice_cost_house(month, year);  // индексация стоимости дома
 
-    bob_salary(month, year); // зп
-    bob_expenses(month, year); // расходы
-    bob_deposit(month, year); // вклад под проценты
+    bob_salary(month, year);  // зп
+    bob_expenses(month, year);  // расходы
+    bob_cat(month, year);
+    bob_deposit(month, year);  // вклад под проценты
     
     month++;
-    if (month == 13) {
+    if (month == 13) 
+    {
         year++;
         month = 1;
     }
@@ -165,11 +208,14 @@ void simulation() {
 }
 
 
-void result() {
-  if (Alice.deposit > Bob.deposit) {
+void result() 
+{
+  if (Alice.deposit > Bob.deposit) 
+  {
     printf("Alice is richer on %lld\n", Alice.deposit - Bob.deposit);
   }
-  else {
+  else 
+  {
     printf("Bob is richer on %lld\n", Bob.deposit - Alice.deposit);
   }
 }
