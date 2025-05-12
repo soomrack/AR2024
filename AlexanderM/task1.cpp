335aa15cf301bfc705d971f010d7bbdfd5fc066f
Alexander598675 <moiseenckovalexander@yandex.ru>
2025-05-12 10:33:27 +0300

    Update task1.cpp CAR

--- AlexanderM/task1.cpp
+++ AlexanderM/task1.cpp
@@ -1,77 +1,118 @@
-#include <stdio.h>
-
-typedef long money;
-
-struct person {
-    money salary;
-    money account;
-    money charges;
-    money rent;
-    money flat;
-    money loan;
-    money unforeseen_situations;
-};
-person Bob{ 200 * 1000, 1000 * 1000, 25 * 1000, 40 * 1000, 0, 100 * 1000, 10 * 1000 };
-person Alice{ 200 * 1000, 1000 * 1000, 30 * 1000, 0, 14000 * 1000, 100 * 1000, 10 * 1000 };
-
-void Bob_salary(const int month, const int year) {
-    if (month == 12) {
-        Bob.salary *= 1.08;
-    }
-    Bob.account += Bob.salary;
-} 
-
-void Bob_charges(const int month, const int year) {
-    if (month == 12) {
-        Bob.rent *= 1.08;
-        Bob.unforeseen_situations *= 1.08;
-    }
-    Bob.charges = Bob.unforeseen_situations + Bob.rent;
-    Bob.account -= Bob.charges;
+#define M1_DIR 4
+#define M1_PWM 5
+#define M2_DIR 7
+#define M2_PWM 6
+int ButtonRead(int ButtonPin)
+{
+  int Data = digitalRead(ButtonPin);
+  if(ButtonPin == 12) Data = !Data;
+  return Data;
 }
 
-void Alice_salary(const int month, const int year) {
-    if (month == 12) {
-        Alice.salary *= 1.08;
-        Alice.flat *= 1.08;
-    }
-    Alice.account += Alice.salary;
+void InitMotors()
+{
+  pinMode(M1_DIR, OUTPUT);
+  pinMode(M1_PWM, OUTPUT);
+  pinMode(M2_DIR, OUTPUT);
+  pinMode(M2_PWM, OUTPUT);
 }
 
-void Alice_charges(const int month, const int year) {
-    if (month == 12) {
-        Alice.unforeseen_situations *= 1.08;
-    }
-    Alice.charges = Alice.unforeseen_situations + Alice.loan;
-    Alice.account -= Alice.charges;
+void Motors(int Speed_1, int Speed_2)
+{
+  if(Speed_1 > 255) Speed_1 = 255;
+  if(Speed_1 < -255) Speed_1 = -255;
+  if(Speed_2 > 255) Speed_2 = 255;
+  if(Speed_2 < -255) Speed_2 = -255;
+
+  if(Speed_1 > 0)
+  {
+    digitalWrite(M1_DIR, 1);
+    analogWrite(M1_PWM, Speed_1);
+  }
+  else
+  {
+    digitalWrite(M1_DIR, 0);
+    analogWrite(M1_PWM, -Speed_1);
+  }
+
+  if(Speed_2 > 0)
+  {
+    digitalWrite(M2_DIR, 1);
+    analogWrite(M2_PWM, Speed_2);
+  }
+  else
+  {
+    digitalWrite(M2_DIR, 0);
+    analogWrite(M2_PWM, -Speed_2);
+  }
 }
 
-int year;
-int month;
-
-void simulation() {
-    while (!(year == 2054 && month == 10)) {
-        Alice_salary(month, year);
-        Alice_charges(month, year);
-        Bob_salary(month, year);
-        Bob_charges(month, year);
-        month++;
-        if (month == 13) {
-            year = year + 1;
-            month = 1;
-        }
-    }
+//Ехать вперед
+void MoveForward(int Speed)
+{
+  Motors(Speed, Speed);
 }
 
-void result() {
-    if (Bob.account > Alice.account) {
-        printf("Bob is richer\n");
-    } else {
-        printf("Alice is richer\n");
-    }
+int LineSensorRead(int SensorPin)
+{
+  if(SensorPin < A0 || SensorPin > A3)
+    return -1;
+
+  int Data = analogRead(SensorPin);
+  return Data;
 }
-int main() {
-    simulation();
-    result();
-    return 0;
+
+void TurnLeft(int Speed)
+{
+  Motors(-Speed, Speed);
+}
+
+void TurnRight(int Speed)
+{
+  Motors(Speed, -Speed);
+}
+
+void Stop()
+{
+  Motors(0, 0);
+}
+
+
+void setup()
+{
+  pinMode(12, INPUT_PULLUP);
+  InitMotors();
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  while (ButtonRead(12))
+  {
+    do
+    {
+      MoveForward(100);
+
+      int leftSensor = LineSensorRead(A2);
+      int rightSensor = LineSensorRead(A3);
+
+      if(leftSensor > 900)
+      {
+        TurnRight(80);
+        delay(200);
+      }
+      else if(rightSensor > 900)
+      {
+        TurnLeft(80);
+        delay(200);
+      }
+    }
+    while(LineSensorRead(A2) < 900 && LineSensorRead(A3) < 900);
+    Stop();
+    delay(100);	
+  }
+
+  Serial.print("message");
+  Serial.print(LineSensorRead(A2));
+  Serial.println();
 }
