e1e56a523b40132c31b121c7bd39b1793bec032f
NikitaKrasnov06 <krasnov_nikita_a06@mail.ru>
2025-05-18 17:45:31 +0300

    Create Greenhouse_Kr.ino

---
+++ Краснов Никита/Greenhouse_Kr.ino
@@ -0,0 +1,128 @@
+#include <DHT11.h>
+
+#define ANALOG_PIN_PHOTO_SENSOR A0
+#define ANALOG_PIN_SOILHUMIDITY A1
+#define PIN_HEATER 4
+#define PIN_PUMP 5
+#define PIN_LAMP 6
+#define PIN_FAN 7
+
+
+DHT11 PIN_DHT(12);
+
+
+int temperature;
+int light;
+int soilhumidity;
+int humidity;
+int t1;
+int t2;
+
+
+bool heater;
+bool lamp;
+bool pump;
+bool fan;
+
+
+void init_data()
+{
+  t1 = 0;
+  pinMode(ANALOG_PIN_PHOTO_SENSOR, INPUT);
+  pinMode(ANALOG_PIN_SOILHUMIDITY, INPUT);
+  pinMode(PIN_HEATER, OUTPUT);
+  pinMode(PIN_LAMP, OUTPUT);
+  pinMode(PIN_PUMP, OUTPUT);
+  pinMode(PIN_FAN, OUTPUT);
+}
+
+
+void sensor_check() 
+ {
+   PIN_DHT.readTemperatureHumidity(temperature, humidity);
+   light = analogRead(ANALOG_PIN_PHOTO_SENSOR);
+   soilhumidity = analogRead(ANALOG_PIN_SOILHUMIDITY);
+ }
+
+
+void heater_control()
+{
+  if (temperature < 25 ) heater = true;
+  else heater = false;
+}
+
+
+void lamp_control()
+{
+  if (light < 700) lamp = true;
+  else lamp = false;
+}
+
+
+void pump_control()
+{
+  t2 = mills();
+
+  if (t1 > t2){
+    t1 -= 4 294 967 295;
+  }
+
+  if (soilhumidity < 70 && t2 - t1 >= 20000){
+   pump = true;
+   t1 = t2; 
+  }
+  else pump = false;
+}
+
+
+void fan_control()
+{
+  if (temperature > 30 || humidity > 90) fan = true;
+  else fan = false;
+}
+
+
+void setup() 
+{
+  init_data();
+  Serial.begin(9600);
+}
+
+
+void system_regulator()
+{
+  if (heater) digitalWrite(PIN_HEATER, HIGH);
+  else digitalWrite(PIN_HEATER, LOW);
+  if (lamp) digitalWrite(PIN_LAMP, HIGH);
+  else digitalWrite(PIN_LAMP, LOW);
+  if (pump) digitalWrite(PIN_PUMP, HIGH);
+  else digitalWrite(PIN_PUMP, LOW);
+  if (fan) digitalWrite(PIN_FAN, HIGH);
+  else digitalWrite(PIN_FAN, LOW);
+}
+
+
+void print_sensor_indicators()
+{
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.print(" °C   Humidity: ");
+  Serial.print(humidity);
+  Serial.print(" %   Light: ");
+  Serial.print(light);
+  Serial.print("   Soilhumidity: ");
+  Serial.println(soilhumidity);
+}
+
+
+void loop() {
+  sensor_check();
+  heater_control();
+  pump_control();
+  lamp_control();
+  fan_control();
+  print_sensor_indicators();
+  system_regulator();
+  delay(10000);
+};
+
